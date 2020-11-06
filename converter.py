0905de7236741f0e71ea204256a5a9062516edb9
Burak Karaca <62096283+burak-karaca@users.noreply.github.com>
2020-11-06 18:46:50 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,7 +2,7 @@
 from PIL import Image
 import pytesseract
 
-class imageProcessing:
+class ImageProcessing:
     def getImage():
         myImage = 'train_5.eps' # insert the eps file to be processed here,it should be in the same path as the python file
         image_eps = myImage
@@ -13,4 +13,4 @@ class imageProcessing:
         img = Image.open('train_1.png')  # the file to be processed
         print(pytesseract.image_to_string(img, config='--psm 6'))
 
-imageProcessing.getImage()
+ImageProcessing.getImage()
